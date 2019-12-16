b727e07f76e0218a61cb6c4580191cc0ac8e2eda
Snowman <snowman@gimmingiui-MacBook-Pro.local>
2019-12-16 22:32:01 +0900

    지은이 누나 파일 합침

--- src/Room.js
+++ src/Room.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import logo from './Logo.png';
 import sejong from './Sejong.png';
@@ -12,109 +12,85 @@ import './Room.css';
 const APP_KEY = "80BDA3A34160D126F3FB4094CBE073EF"
 
 export default function Rank() {
-
     const [word, setWord] = useState(null);
-    const onSend = (e) => {
-        e.preventDefault();
-        if (!word) {
-            return alert('단어를 입력하세요.');
+      const convert = require('xml-js');
+
+    const timer = (sec) => {
+        let timer = document.getElementById("timer")
+        timer.innerHTML = sec
+        setInterval(() => {      
+        timer.innerHTML -= 1;
+        }, 1000)
+    }
+
+    const checkWord = (e) => {
+        e.preventDefault(); 
+        document.getElementById("wordBox").value = ""
+        let inputConsonant = checkChosung(word);
+        let consonant = document.getElementById('consonant').innerHTML
+        if (inputConsonant === consonant) {
+        console.log("자음 일치!") 
+        axios.get(`https://cors-anywhere.herokuapp.com/https://krdict.korean.go.kr/api/search?certkey_no=1154&key=${APP_KEY}&type_search=search&method=WORD_INFO&part=word&q=${word}&sort=dict`, {
+        })
+        .then(response => {
+            const result = convert.xml2json(response.data, {compact: true, spaces: 4});
+            return JSON.parse(result)
+        })
+        .then(response => {
+            if (response.channel.item) {
+            console.log("PASS")
+            if ((response.channel.item).length > 1) {
+                if ((response.channel.item[0].sense).length > 1) {
+                console.log(response.channel.item[0].sense[0].definition._text)
+                }
+                else {
+                console.log(response.channel.item[0].sense.definition._text)
+                }
+            } else if ((response.channel.item).length === 1) {
+                console.log(response.channel.item.sense.definition._text)
+            } else {
+                console.log("오, 이런 어려운 단어도 알다니! 아주 칭찬해~")
+            }
+            } else {
+            console.log("WRONG")
+            }
+        })
+        } else {
+        console.log("자음 불일치!") 
         }
     }
-    const [word, setWord] = useState(null)
-  const convert = require('xml-js');
 
-  const timer = (sec) => {
-    let timer = document.getElementById("timer")
-    timer.innerHTML = sec
-    setInterval(() => {      
-      timer.innerHTML -= 1;
-    }, 1000)
-  }
+    const randomChosung = (n) => {
+        const consonantList = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ", "ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
+        const shuffleConsonants = shuffle(consonantList);
+        console.log(shuffleConsonants);
+        const consonants = shuffleConsonants.slice(0, n).join("")
+        document.getElementById('consonant').innerHTML = consonants
+    }
 
-  const checkWord = (e) => {
-    e.preventDefault(); 
-    document.getElementById("wordBox").value = ""
-    let inputConsonant = checkChosung(word);
-    let consonant = document.getElementById('consonant').innerHTML
-    if (inputConsonant === consonant) { // 자음이 일치하면, 사전등록단어인지 체크
-      console.log("자음 일치!") 
-      // CORS 해결 방법 변경
-      axios.get('https://cors-anywhere.herokuapp.com/' + `https://krdict.korean.go.kr/api/search?certkey_no=1154&key=${APP_KEY}&type_search=search&method=WORD_INFO&part=word&q=${word}&sort=dict`, {
-        // function body 
-      })
-      .then(response => {
-        // console.log(response.data)
-        const result = convert.xml2json(response.data, {compact: true, spaces: 4}); // compact: bool
-        return JSON.parse(result)
-        // console.log(JSON.parse(result));
-      })
-      .then(response => {
-        // console.log(response.channel.item)
-        if (response.channel.item) { // 사전 등재 단어라면
-          console.log("PASS") // PASS
-          if ((response.channel.item).length > 1) {
-            if ((response.channel.item[0].sense).length > 1) {
-              console.log(response.channel.item[0].sense[0].definition._text)
-            }
-            else {
-              console.log(response.channel.item[0].sense.definition._text)
-            }
-          } else if ((response.channel.item).length === 1) {
-              console.log(response.channel.item.sense.definition._text)
-          } else {
-              console.log("오, 이런 어려운 단어도 알다니! 아주 칭찬해~")
-          }
-        } else { // 사전 등재 단어가 아니라면
-          console.log("WRONG")
+    const shuffle = (a) => {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
         }
-      })
-    } else { // 자음이 일치하지 않는다면
-      console.log("자음 불일치!") 
+        return a;
     }
-  }
 
-  const randomChosung = (n) => {
-    let consonantList = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ", "ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
-    const shuffleConsonants = shuffle(consonantList);
-    console.log(shuffleConsonants);
-    // const rdm = Math.floor(Math.random() * shuffleConsonants.length)
-    const consonants = shuffleConsonants.slice(0, n).join("")
-    // console.log(consonants)
-    document.getElementById('consonant').innerHTML = consonants
-  }
+    useEffect(() => {
+        console.log("set new chosung")
+        randomChosung(2)  
+        timer(60)
+    }, [])
 
-  const shuffle = (a) => {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+    const checkChosung = (str) => {
+        const cho = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
+        let result = "";
+        for(let i=0; i<str.length; i++) {
+        let code = str.charCodeAt(i)-44032;
+        if(code>-1 && code<11172) result += cho[Math.floor(code/588)];
+        }
+        return result;
     }
-    return a;
-  }
-
-  // useEffect 잘 모름
-  useEffect(() => {
-    console.log("set new chosung")
-    randomChosung(2)  
-    timer(60)
-  }, [])
-
-  const checkChosung = (str) => {
-    let cho = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
-    let result = "";
-    for(let i=0; i<str.length; i++) {
-      let code = str.charCodeAt(i)-44032;
-      if(code>-1 && code<11172) result += cho[Math.floor(code/588)];
-    }
-    return result;
-  }
-
-          <span id="consonant" style={{ border: "2px solid white", width: "500px", backgroundColor: "white" }}></span> 
-          <span id="timer" style={{ border: "2px solid white", width: "500px", backgroundColor: "white" }}></span>
-          <form onSubmit={checkWord}>
-            <label style={{ color: "white" }}> 단어를 입력하세요: </label> 
-              <input type="text" id="wordBox" onChange={(e) => {setWord(e.target.value)}} />
-              <input type="submit" value="Submit" />
-          </form>
 
     return (
         <div className="background">  
@@ -131,9 +107,13 @@ export default function Rank() {
             </div>
             <img src={playBox} className="play-box" alt="게임판"/>
             <div className="consonant"></div>
-            <form onSubmit={onSend}>
-                        <input type="text" name="name" placeholder="단어 입력.." onChange={(e) => setWord(e.target.value)} className="send"/>
-                    </form>
+            <span id="consonant"></span> 
+            <span id="rest-time">남은 시간:</span>
+            <span id="timer"></span>
+            <form onSubmit={checkWord}>
+                <label> 단어를 입력하세요: </label> 
+                <input type="text" id="wordBox" placeholder="단어 입력.." onChange={(e) => {setWord(e.target.value)}} />
+            </form>
             <img src={scoreBox} className="score-box" alt="점수판"/>
             <div className="score-list">
                     뿌꾸뿌꾸: 2560점
